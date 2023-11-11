1bb90eeacefa7b964115f1316aa3716d0d237fe6
Joshua Zarazovski <joshuazarazovski@gmail.com>
2023-11-11 19:39:42 +1100

    Alert style and .env added to gitignore

--- app/components/Alert.jsx
+++ app/components/Alert.jsx
@@ -4,17 +4,17 @@ const Alert = ({ type, text }) => {
       <div
         className={`p-2 ${
           type === "danger" ? "bg-pu text-indigo-100" : "bg-cy text-gld"
-        } items-center  leading-none lg:rounded-md flex`}
+        } items-center leading-none rounded-full flex`}
         role="alert"
       >
         <p
           className={`flex rounded-full ${
             type === "danger" ? "bg-pu" : "bg-cy"
-          } flex rounded-md uppercase px-2 py-2 text-sm font-semibold mr-1.5`}
+          } flex rounded-full uppercase px-2 py-2 text-sm font-semibold mr-1.5`}
         >
           {type === "danger" ? "Failed" : "Success"}
         </p>
-        <p className="mr-1 text-left">{text}</p>
+        <p className="mr-0.5 text-left">{text}</p>
       </div>
     </div>
   );
