851dc061917d99cd54088ede480cf0db808e210b
Joshua Zarazovski <joshuazarazovski@gmail.com>
2023-11-11 18:15:56 +1100

    Alert style

--- app/components/Alert.jsx
+++ app/components/Alert.jsx
@@ -4,13 +4,13 @@ const Alert = ({ type, text }) => {
       <div
         className={`p-2 ${
           type === "danger" ? "bg-pu text-indigo-100" : "bg-cy text-gld"
-        } items-center  leading-none lg:rounded-full flex lg:inline-flex`}
+        } items-center  leading-none lg:rounded-md flex`}
         role="alert"
       >
         <p
           className={`flex rounded-full ${
             type === "danger" ? "bg-pu" : "bg-cy"
-          } flex rounded-full uppercase px-2 py-2 text-sm font-semibold mr-1.5`}
+          } flex rounded-md uppercase px-2 py-2 text-sm font-semibold mr-1.5`}
         >
           {type === "danger" ? "Failed" : "Success"}
         </p>
